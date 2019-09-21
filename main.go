166c9e32f34028f003cb4a4cce9194b500f2b380
Wojciech Orzechowski <wojciech.orzechowski@gmail.com>
2019-09-21 14:40:17 +0200

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Worlddssfasdfdfgadfg"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello worlddsl;fdgf'[r - the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
